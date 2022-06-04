6fb9a3f942b50243d579fb914b88f3f0dab263fe
andres holivin <andres12holivin@gmail.com>
2022-06-04 21:53:45 +0700

    Initial Commit

---
+++ CloneTwitter/CloneTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneTwitterApp.swift
+//  CloneTwitter
+//
+//  Created by andres holivin on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CloneTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
